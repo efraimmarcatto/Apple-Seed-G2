f7037732f6481307654642dd7e6df4068b6ecc8a
Efraim Marcatto <efraimmarcatto@gmail.com>
2025-11-08 00:12:39 -0300

    Adds options scene and configuration file
    Resolution settings and music/effect volumes.

--- scenes/ui/main.gd
+++ scenes/ui/main.gd
@@ -2,7 +2,9 @@ extends Control
 
 
 func _ready() -> void:
-	pass
+	var current_resolution = GameManager.resolutions[GameManager.settings.get("resolution", 0)]
+	DisplayServer.window_set_size(current_resolution)
+	get_tree().root.content_scale_size = current_resolution
 
 
 func _quit() -> void:
@@ -10,4 +12,8 @@ func _quit() -> void:
 
 
 func _start_game() -> void:
-	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
+	get_tree().change_scene_to_file("res://levels/game.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui/options/options.tscn")
--- singletons/game_manager.gd
+++ singletons/game_manager.gd
@@ -1,11 +1,33 @@
 extends Node
 
+var settings = {}
+var settings_file = "user://settings.cfg"
+var resolutions = [Vector2i(320,180),Vector2i(320,240)]
 
-# Called when the node enters the scene tree for the first time.
+func load_settings():
+	if !FileAccess.file_exists(settings_file):
+		settings["resolution"] = 0
+		settings["music"] = 1
+		settings["effect"] = 1
+		var file = FileAccess.open(settings_file, FileAccess.WRITE)
+		file.store_var(settings)
+		file.close()
+		return
+	else:
+		var file = FileAccess.open(settings_file, FileAccess.READ)
+		settings = file.get_var()
+		file.close()
+
+func save_settings():
+	var file = FileAccess.open(settings_file, FileAccess.WRITE)
+	file.store_var(settings)
+	file.close()
+	
+
 func _ready() -> void:
-	pass # Replace with function body.
+	load_settings()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(_delta: float) -> void:
 	pass
