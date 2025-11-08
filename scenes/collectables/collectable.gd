66385a9d43ce2e08097637fcee6af51e6049d03b
welton <tomwb1@gmail.com>
2025-11-07 23:51:08 -0300

    feat: collectable throw

--- scenes/collectables/collectable.gd
+++ scenes/collectables/collectable.gd
@@ -1,16 +1,56 @@
 extends Area2D
 class_name Collectable
 
-var is_carried: bool = false
+@export var parent: CharacterBody2D
+@export var collision_shape_2d: CollisionShape2D
+
+@export_category("throw")
+@export var throw_speed: float = 200.0
+@export var throw_time: float = 0.25
+
+enum STATES {DEFAULT,CARRIED,THROWN}
+
+var state: STATES = STATES.DEFAULT
+var throw_direction: Vector2 = Vector2.ZERO
+var root_tree: Node2D
 
 func _ready() -> void:
 	add_to_group(Globals.GROUP_COLLECTABLE)
-
-
-func carry() -> void:
-	is_carried = true
-	z_index = 1
+	if parent:
+		root_tree = parent.get_parent()
 	
-func throw() -> void:
-	is_carried = false
-	z_index = 0
+func _physics_process(_delta: float) -> void:
+	if state == STATES.THROWN and parent:
+		parent.velocity = throw_direction * throw_speed
+		parent.move_and_slide()
+
+func is_carryble() -> bool:
+	return state == STATES.DEFAULT
+
+func is_carried() -> bool:
+	return state == STATES.CARRIED
+
+func carry(new_parent:Node2D, new_position:Vector2) -> void:
+	state = STATES.CARRIED
+	if collision_shape_2d:
+		collision_shape_2d.disabled = true
+	if parent:
+		set_carry_position(new_position)
+		#parent.z_index = 1
+		parent.reparent(new_parent,true)
+
+func set_carry_position(new_position:Vector2) -> void:
+	if parent:
+		parent.global_position = new_position
+
+func throw(direction: Vector2 = Vector2.RIGHT) -> void:
+	state = STATES.THROWN
+	throw_direction = direction
+	if parent:
+		await get_tree().create_timer(0.05).timeout
+		parent.reparent(root_tree,true)
+	if collision_shape_2d:
+		collision_shape_2d.disabled = false
+		
+	await get_tree().create_timer(throw_time).timeout
+	state = STATES.DEFAULT
--- scenes/player/carry_controller.gd
+++ scenes/player/carry_controller.gd
@@ -2,6 +2,9 @@ extends Area2D
 class_name CarryController
 
 @export var carry_marker_2d: Marker2D
+@export var player: Player
+@export var character_movement_controller: CharacterMovementController
+
 
 var carrying_collectable: Collectable = null ## item carregado
 var near_collectable: Collectable = null ## coletavel em frente
@@ -15,26 +18,33 @@ func _process(_delta: float) -> void:
 		pick_collectable()
 	if Input.is_action_just_released('key_action'):
 		throw_collectable()
-	
-	if carrying_collectable and is_instance_valid(carrying_collectable) and carry_marker_2d:
-		carrying_collectable.global_position = carry_marker_2d.global_position
+		
+	if carrying_collectable and is_instance_valid(carrying_collectable) and carrying_collectable.has_method("set_carry_position"):
+		carrying_collectable.set_carry_position(carry_marker_2d.global_position)
 
 func is_carrying() -> bool:
-	return (carrying_collectable and is_instance_valid(carrying_collectable)) or Input.is_action_pressed("key_action")
+	return (carrying_collectable and is_instance_valid(carrying_collectable) and carrying_collectable.has_method("is_carried") and carrying_collectable.is_carried()) or Input.is_action_pressed("key_action")
 
 func pick_collectable() -> void:
-	if near_collectable and is_instance_valid(near_collectable):
+	if near_collectable and is_instance_valid(near_collectable) and near_collectable.has_method("is_carryble") and near_collectable.is_carryble():
 		carrying_collectable = near_collectable
 		near_collectable = null
-		carrying_collectable.carry()
+		carrying_collectable.carry(player, carry_marker_2d.global_position)
 
 func throw_collectable() -> void:
 	if carrying_collectable and is_instance_valid(carrying_collectable):
-		carrying_collectable.throw()
-		carrying_collectable = null
+		if character_movement_controller:
+			carrying_collectable.throw(character_movement_controller.last_movement_direction)
+	carrying_collectable = null
+	# remove a colisão do player com o item temporariamente
+	if player:
+		player.set_collision_mask_value(5, false)
+		await get_tree().create_timer(0.1).timeout
+		player.set_collision_mask_value(5, true)
+	
 
 func _on_area_entered(area:Area2D) -> void:
-	if area.is_in_group(Globals.GROUP_COLLECTABLE):
+	if area.is_in_group(Globals.GROUP_COLLECTABLE) and area.has_method("is_carryble") and area.is_carryble():
 		near_collectable = area
 
 func _on_area_exited(area:Area2D) -> void:
